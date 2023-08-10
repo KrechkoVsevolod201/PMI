3399deaeb815e7fb1f2591babe57a6a8dbb8ac69
Sharky_001 <71152134+KrechkoVsevolod201@users.noreply.github.com>
2023-08-10 08:29:00 +0400

    +

--- num62.py
+++ num62.py
@@ -8,26 +8,42 @@ class LinkedList:
     def __init__(self):
         self.head = None
 
-    def insert(self, data):
-        newNode = Node(data)
-        if (self.head):
-            current = self.head
-            while (current.next):
-                current = current.next
-            current.next = newNode
-        else:
-            self.head = newNode
+    def insert(self, new_data):
+        new_node = Node(new_data)
+        new_node.next = self.head
+        self.head = new_node
 
-    def printLL(self):
-        current = self.head
-        while (current):
-            print(current.data)
-            current = current.next
+    def deleteNode(self, key):
+        temp = self.head
+        if (temp is not None):
+            if (temp.data == key):
+                self.head = temp.next
+                temp = None
+                return
+        while (temp is not None):
+            if temp.data == key:
+                break
+            prev = temp
+            temp = temp.next
+        if (temp == None):
+            return
+        prev.next = temp.next
+        temp = None
+
+    def printList(self):
+        temp = self.head
+        while(temp):
+            print (" %d" %(temp.data)),
+            temp = temp.next
 
 
 if __name__ == '__main__':
     L = LinkedList()
+    delete_item = 3
     L.insert(3)
     L.insert(4)
     L.insert(5)
-    L.printLL()
+    L.printList()
+    L.deleteNode(delete_item)
+    print(f"Список после удаления элемента:  {delete_item}:")
+    L.printList()
